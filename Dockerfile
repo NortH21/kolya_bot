1930156369b3818e7a12fdd74e200235ace044a4
Lebedev Yaroslav <y.lebedev@corp.mail.ru>
2023-12-28 22:20:31 +0300

    Init

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM golang:latest as builder
+
+WORKDIR /build
+
+COPY go.mod go.sum ./
+
+RUN go mod download
+
+COPY . .
+
+RUN go build -o bot -ldflags '-linkmode external -w -extldflags "-static"' .
+
+
+FROM alpine:latest  
+
+RUN apk --no-cache add ca-certificates
+
+WORKDIR /app
+
+COPY --from=builder /build/bot .
+
+CMD ["/app/bot"]
---
+++ app.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"os"
+	"log"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	u.AllowedUpdates = []string{"channel_post", "message"}
+
+	updates := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.ChannelPost != nil {
+			channelMsg := update.ChannelPost
+			if bot.Debug {
+				log.Printf("Channel: [%s] %s", channelMsg.Chat.UserName, channelMsg.Text)
+			}
+		} else if update.Message != nil {
+			groupMsg := update.Message
+			if bot.Debug {
+				log.Printf("Group: [%s] %s", groupMsg.Chat.UserName, groupMsg.Text)
+			}
+			if strings.ToLower(update.Message.Text) == "да" {
+				reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Пизда")
+				_, err := bot.Send(reply)
+				if err != nil {
+					log.Println(err)
+				}
+			} else if strings.ToLower(update.Message.Text) == "нет" {
+				reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Пидора ответ")
+				_, err := bot.Send(reply)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}
+}
