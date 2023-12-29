e93cb624d52df9212aeed4e321b3d7acb123ff58
Ramil4ik <44543538+Ramil4ik@users.noreply.github.com>
2023-12-29 16:54:02 +0400

    Update app.go
    Отвечает на "да" и "нет" в независимости от регистра написания
    Периодически шлёт сообщения, если долго никто не писал

--- app.go
+++ app.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"os"
 	"log"
+	"os"
 	"strings"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
-
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
@@ -18,6 +18,9 @@ func main() {
 	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Переменная для хранения времени последнего сообщения
+	var lastMessageTime time.Time
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -30,27 +33,50 @@ func main() {
 
 	for update := range updates {
 		if update.ChannelPost != nil {
-			channelMsg := update.ChannelPost
-			if bot.Debug {
-				log.Printf("Channel: [%s] %s", channelMsg.Chat.UserName, channelMsg.Text)
-			}
+			// Обработка постов в каналах
 		} else if update.Message != nil {
-			groupMsg := update.Message
-			if bot.Debug {
-				log.Printf("Group: [%s] %s", groupMsg.Chat.UserName, groupMsg.Text)
-			}
-			if strings.ToLower(update.Message.Text) == "да" {
+			// Обработка сообщений в группах/чатах
+			// Преобразование текста сообщения в нижний регистр для сравнения
+			lowerText := strings.ToLower(update.Message.Text)
+
+			// Проверка, что "да" встречается в конце предложения
+			if strings.HasSuffix(lowerText, "да") {
 				reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Пизда")
 				_, err := bot.Send(reply)
 				if err != nil {
 					log.Println(err)
 				}
-			} else if strings.ToLower(update.Message.Text) == "нет" {
+
+				// Обновление времени последнего сообщения
+				lastMessageTime = time.Now()
+			} else if strings.HasSuffix(lowerText, "нет") {
 				reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Пидора ответ")
 				_, err := bot.Send(reply)
 				if err != nil {
 					log.Println(err)
 				}
+
+				// Обновление времени последнего сообщения
+				lastMessageTime = time.Now()
+			}
+		}
+
+		// Проверка на отправку сообщения "Есть живые тут?" после 6 часов без активности
+		if time.Since(lastMessageTime).Hours() >= 6 {
+			// Проверка, что текущее время не в интервале 23:00 - 09:00 по Москве
+			loc, _ := time.LoadLocation("Europe/Moscow")
+			currentTime := time.Now().In(loc)
+
+			if currentTime.Hour() >= 9 && currentTime.Hour() < 23 {
+				// Отправка сообщения
+				reply := tgbotapi.NewMessage(update.Message.Chat.ID, "Есть живые тут? Как дела в целом?")
+				_, err := bot.Send(reply)
+				if err != nil {
+					log.Println(err)
+				}
+
+				// Обновление времени последнего сообщения
+				lastMessageTime = time.Now()
 			}
 		}
 	}
