064f91c6eb5b87ffdc6a0897d184f7470435197b
Lebedev Yaroslav <y.lebedev@corp.mail.ru>
2024-03-08 13:51:38 +0300

    Added forecast for God city

--- app.go
+++ app.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -31,57 +31,52 @@ var (
 	meetUrl = "https://jitsi.sipleg.ru/spd"
 )
 
+func getTemperature(city string) (int, int, int, int, error) {
+	type Forecast struct {
+		Forecastday []struct {
+			Day struct {
+				MaxTempC float64 `json:"maxtemp_c"`
+				MinTempC float64 `json:"mintemp_c"`
+				AvgTempC float64 `json:"avgtemp_c"`
+			} `json:"day"`
+		} `json:"forecastday"`
+	}
 
-func getTemperature(city string) (string, error) {
-    type Forecast struct {
-        Forecastday []struct {
-            Day struct {
-                MaxTempC float64 `json:"maxtemp_c"`
-                MinTempC float64 `json:"mintemp_c"`
-                AvgTempC float64 `json:"avgtemp_c"`
-            } `json:"day"`
-        } `json:"forecastday"`
-    }
-	
 	type Current struct {
 		TempC float64 `json:"temp_c"`
 	}
-	
+
 	type WeatherData struct {
-		Current   Current    `json:"current"`
-        Forecast  Forecast   `json:"forecast"`
+		Current  Current  `json:"current"`
+		Forecast Forecast `json:"forecast"`
 	}
-	
+
 	url := "https://api.weatherapi.com/v1/forecast.json?q=" + city + "&days=1&key=" + os.Getenv("weatherapi_key")
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return 0, 0, 0, 0, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return 0, 0, 0, 0, err
 	}
 
 	var weatherData WeatherData
 	err = json.Unmarshal(body, &weatherData)
 	if err != nil {
-		return "", err
+		return 0, 0, 0, 0, err
 	}
 
-    maxTemp := weatherData.Forecast.Forecastday[0].Day.MaxTempC
-    minTemp := weatherData.Forecast.Forecastday[0].Day.MinTempC
-    avgTemp := weatherData.Forecast.Forecastday[0].Day.AvgTempC
+	maxTemp := weatherData.Forecast.Forecastday[0].Day.MaxTempC
+	minTemp := weatherData.Forecast.Forecastday[0].Day.MinTempC
+	avgTemp := weatherData.Forecast.Forecastday[0].Day.AvgTempC
 	curTemp := weatherData.Current.TempC
 
-	curStr := fmt.Sprintf("Текущая температура %d°C. Сегодня днем до %d°C, в среднем %d°C, ночью до %d°C. В Баку прекрасная погода.",
-		int(curTemp), int(maxTemp), int(avgTemp), int(minTemp))
-
-	return curStr, nil
+	return int(curTemp), int(minTemp), int(avgTemp), int(maxTemp), err
 }
-
 
 func shouldSendReply(chatID int64) bool {
 	currentTime := time.Now()
@@ -163,12 +158,22 @@ func sendMorningGreetings(bot *tgbotapi.BotAPI) {
 		log.Println(err)
 	}
 
-	temp, err := getTemperature("Yaroslavl")
+	curTempYar, minTempYar, avgTempYar, maxTempYar, err := getTemperature("Yaroslavl")
 	if err != nil {
 		log.Println(err)
 	}
+	tempYar := fmt.Sprintf("В одном из старейших русских городов, основанный в XI веке и достигший своего расцвета в XVII веке, сейчас %d°C. Сегодня тепература будет до %d°C, в среднем %d°C и минимально до %d°C.",
+		curTempYar, maxTempYar, avgTempYar, minTempYar)
 
-	forecast := tgbotapi.NewMessage(reminderChatID, temp)
+	curTempBak, minTempBak, avgTempBak, maxTempBak, err := getTemperature("Baku")
+	if err != nil {
+		log.Println(err)
+	}
+	tempBak := fmt.Sprintf("На Апшеронском полуострове в городе Бога сегодня тоже прекрасная погода, сейчас %d°C. Днем до %d°C, в среднем %d°C, ночью до %d°C.",
+		curTempBak, maxTempBak, avgTempBak, minTempBak)
+
+	fullForecast := fmt.Sprintf("%s \n\n%s", tempYar, tempBak)
+	forecast := tgbotapi.NewMessage(reminderChatID, fullForecast)
 	_, err = bot.Send(forecast)
 	if err != nil {
 		log.Println(err)
@@ -303,12 +308,23 @@ func main() {
 							log.Println(err)
 						}
 					case "/forecast":
-						temp, err := getTemperature("Yaroslavl")
+						curTempYar, minTempYar, avgTempYar, maxTempYar, err := getTemperature("Yaroslavl")
 						if err != nil {
 							log.Println(err)
 						}
+						tempYar := fmt.Sprintf("В одном из старейших русских городов, основанный в XI веке и достигший своего расцвета в XVII веке, сейчас %d°C. Сегодня тепература будет до %d°C, в среднем %d°C и минимально до %d°C.",
+							curTempYar, maxTempYar, avgTempYar, minTempYar)
 
-						reply := tgbotapi.NewMessage(chatID, temp)
+						curTempBak, minTempBak, avgTempBak, maxTempBak, err := getTemperature("Baku")
+						if err != nil {
+							log.Println(err)
+						}
+						tempBak := fmt.Sprintf("На Апшеронском полуострове в городе Бога сегодня тоже прекрасная погода, сейчас %d°C. Днем до %d°C, в среднем %d°C, ночью до %d°C.",
+							curTempBak, maxTempBak, avgTempBak, minTempBak)
+					
+						fullForecast := fmt.Sprintf("%s \n\n%s", tempYar, tempBak)
+
+						reply := tgbotapi.NewMessage(chatID, fullForecast)
 						_, err = bot.Send(reply)
 						if err != nil {
 							log.Println(err)
